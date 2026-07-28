0a406d7832f8b408dcb3fc984ae289292b670449
Your Name <you@example.com>
2026-07-28 12:30:39 +0300

    feat: Migrate Terraform local state to Azure Blob remote backend with state locking

--- providers.tf
+++ providers.tf
@@ -7,7 +7,15 @@ terraform {
             version = "~> 3.0"
         }
     }
+
+    backend "azurerm" {
+        resource_group_name  = "rg-tfstate-lab"
+        storage_account_name = "tfstate1599118008"
+        container_name       = "tfstate"
+        key                  = "hub-spoke.terraform.tfstate"
+    }
 }
+
 provider "azurerm" {
     features{}
 }
